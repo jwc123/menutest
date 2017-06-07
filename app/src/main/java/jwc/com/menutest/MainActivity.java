d8e0b3322ef14a78ac8187674d9ca2e9526ef43a
JWC <348987114@qq.com>
2017-06-07 09:58:07 +0800

    Initial commit

---
+++ app/src/main/java/jwc/com/menutest/MainActivity.java
@@ -0,0 +1,20 @@
+package jwc.com.menutest;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Gson aa=new Gson();
+    }
+
+}
